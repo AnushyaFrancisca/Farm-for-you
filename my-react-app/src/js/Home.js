3405743ec6ae981380bd4432fdb59421b70dc5f0
AnushyaFranscisca <anushya.francisca@msds.christunniversity.in>
2024-09-03 10:21:38 +0530

    home page temporary images uploaded

--- my-react-app/src/js/Home.js
+++ my-react-app/src/js/Home.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import '../css/Home.css'; // Adjust the path if necessary
+import myImage from '../images/bg-img.png'; // Adjust the path to your image
+import cloudImage from '../images/clouds.png'; // The cloud image you want to use
+
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the home page of your React app.</p>
+    <div className="home-container" style={{ backgroundImage: `url(${myImage})` }}>
+      <img src={cloudImage} alt="Moving Clouds" className="moving-clouds" />
+
+      {/* Main Content */}
+      <div className="content" style={{ position: 'relative', zIndex: 2 }}>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home page of your React app.</p>
+      </div>
     </div>
   );
 }
+
 
 export default Home;
 
