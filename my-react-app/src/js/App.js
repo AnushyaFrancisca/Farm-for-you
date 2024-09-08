07fde0eec6b62f414c6bf565dd20b4c74165c6e2
Britto69 <britto2277@gmail.com>
2024-09-08 22:12:29 +0530

    about us page templete

--- my-react-app/src/js/App.js
+++ my-react-app/src/js/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import SignUp from './SignUp';
+import About from './About';
 import logo from '../images/logo.svg'; // Update the path to where your logo.svg is located
 import '../css/App.css'; // Update the path to where your App.css is located
 
@@ -10,8 +11,11 @@ function App() {
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<About />} />
+          
+
         </Routes>
       </div>
     </Router>
