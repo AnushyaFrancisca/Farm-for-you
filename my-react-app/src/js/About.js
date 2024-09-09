d36d9a25f9c276b3bf0e30ff2a00a2a467e7bcda
AnushyaFranscisca <anushya.francisca@msds.christunniversity.in>
2024-09-09 13:29:18 +0530

    errors are commented

--- my-react-app/src/js/About.js
+++ my-react-app/src/js/About.js
@@ -1,22 +1,22 @@
-import '../css/about.css';
-import Homepg from '../Components/Homepg';
-import Aboutpg from '../Components/aboutpg';
-import Work from '../Components/Work';
-import Testimonial from '../Components/Testimonial';
-import Contact from '../Components/Contact';
-import Footer from '../Components/Footer';
+// import '../css/about.css';
+// import Homepg from '../Components/Homepg';
+// import Aboutpg from '../Components/aboutpg';
+// import Work from '../Components/Work';
+// import Testimonial from '../Components/Testimonial';
+// import Contact from '../Components/Contact';
+// import Footer from '../Components/Footer';
 
-function About() {
-  return (
-    <div className="App">
-      <Homepg />
-      <Aboutpg />
-      <Work />
-      <Testimonial />
-      <Contact />
-      <Footer />
-    </div>
-  );
-}
+// function About() {
+//   return (
+//     <div className="App">
+//       <Homepg />
+//       <Aboutpg />
+//       <Work />
+//       <Testimonial />
+//       <Contact />
+//       <Footer />
+//     </div>
+//   );
+// }
 
-export default About;
+// export default About;
--- my-react-app/src/js/App.js
+++ my-react-app/src/js/App.js
@@ -3,12 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import SignUp from './SignUp/SignUp';
 import SignIn from './SignIn/SignIn';
-<<<<<<< HEAD
 import Dashboard from './Dashboard/Dashboard';
-// import About from './About';
-=======
 import About from './About';
->>>>>>> 5a628a35b6b86ca9d3cb425871d1fcb06a73d4c3
 import logo from '../images/logo.svg'; // Update the path to where your logo.svg is located
 import '../css/App.css'; // Update the path to where your App.css is located
 
@@ -17,7 +13,7 @@ function App() {
     <Router>
       <div>
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} /> 
           {/* <Route path="/" element={<Dashboard />} />   */}
