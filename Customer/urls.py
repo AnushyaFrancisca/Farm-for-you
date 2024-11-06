c2fc54936e864bd572e336c4344ed6c0f8ad2d38
AnushyaFranscisca <anushya.francisca@msds.christunniversity.in>
2024-11-07 00:03:05 +0530

    Adding item to cart and calculating total cost

--- Customer/urls.py
+++ Customer/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from Customer.views import *
 
 urlpatterns = [
-    path('',profile,name='others-profile'),    
-    path('customer_market/',customer_market,name='customer_market'),   
-    path('cart/',cart,name='cart'),    
- 
-]
+    path('', profile, name='others-profile'),
+    path('customer_market/', customer_market, name='customer_market'),
+    path('cart/', view_cart, name='view_cart'),
+    path('add/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('remove/<int:item_id>/', remove_from_cart, name='remove_from_cart'),
+    ]
--- Customer/views.py
+++ Customer/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # views.py
 
 from django.shortcuts import render
@@ -13,5 +13,26 @@ def customer_market(request):
     products = Product.objects.all()  # Fetch all products
     return render(request, 'Customer/customer_market.html', {'products': products})
 
-def cart(request):
-    return render(request, 'Customer/cart.html')
+
+from .models import CartItem
+from Farmer.models import Product
+
+
+def view_cart(request):
+    cart_items = CartItem.objects.filter(user=request.user)
+    total_price = sum(item.product.price * item.quantity for item in cart_items)
+    return render(request, 'Customer/cart.html', {'cart_items': cart_items, 'total_price': total_price})
+
+def add_to_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    cart_item, created = CartItem.objects.get_or_create(product=product, 
+                                                       user=request.user)
+    cart_item.quantity += 1
+    cart_item.save()
+    return redirect('view_cart')
+
+def remove_from_cart(request, item_id):
+    cart_item = CartItem.objects.get(id=item_id)
+    cart_item.delete()
+    return redirect('view_cart')
+
